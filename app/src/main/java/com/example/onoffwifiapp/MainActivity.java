3a01a30f1f87f089696090cd701ee070cbcf8194
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-03 20:06:16 +0530

    get systemservice for wifi.

--- app/src/main/java/com/example/onoffwifiapp/MainActivity.java
+++ app/src/main/java/com/example/onoffwifiapp/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.onoffwifiapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button b1,b2;
+    WifiManager wf;
+    @SuppressLint("WifiManagerLeak")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1 = findViewById(R.id.button);
+        b2 = findViewById(R.id.button2);
+        wf = (WifiManager) getSystemService(WIFI_SERVICE);
+
     }
 }
